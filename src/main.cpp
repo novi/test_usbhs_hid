b77b3bd329416e5811e3b40f05aa7bc81e65448d
Yusuke Ito <novi.mad@gmail.com>
2020-07-20 00:21:40 +0900

    USB hub, 1 mouse, 1 keyboard

---
+++ src/main.cpp
@@ -0,0 +1,271 @@
+// #define DEBUG_USB_HOST 0
+
+#include <Arduino.h>
+#include <hidboot.h>
+#include <usbhub.h>
+#include <SPI.h>
+// #include <hidcomposite.h>
+// #include <hiduniversal.h>
+// #include <hidescriptorparser.h>
+
+class MouseRptParser : public MouseReportParser
+{
+  protected:
+    void OnMouseMove(MOUSEINFO *mi);
+    void OnLeftButtonUp(MOUSEINFO *mi);
+    void OnLeftButtonDown(MOUSEINFO *mi);
+    void OnRightButtonUp(MOUSEINFO *mi);
+    void OnRightButtonDown(MOUSEINFO *mi);
+    void OnMiddleButtonUp(MOUSEINFO *mi);
+    void OnMiddleButtonDown(MOUSEINFO *mi);
+};
+void MouseRptParser::OnMouseMove(MOUSEINFO *mi)
+{
+  Serial.print("dx=");
+  Serial.print(mi->dX, DEC);
+  Serial.print(" dy=");
+  Serial.println(mi->dY, DEC);
+};
+void MouseRptParser::OnLeftButtonUp	(MOUSEINFO *mi)
+{
+  Serial.println("L Butt Up");
+};
+void MouseRptParser::OnLeftButtonDown	(MOUSEINFO *mi)
+{
+  Serial.println("L Butt Dn");
+};
+void MouseRptParser::OnRightButtonUp	(MOUSEINFO *mi)
+{
+  Serial.println("R Butt Up");
+};
+void MouseRptParser::OnRightButtonDown	(MOUSEINFO *mi)
+{
+  Serial.println("R Butt Dn");
+};
+void MouseRptParser::OnMiddleButtonUp	(MOUSEINFO *mi)
+{
+  Serial.println("M Butt Up");
+};
+void MouseRptParser::OnMiddleButtonDown	(MOUSEINFO *mi)
+{
+  Serial.println("M Butt Dn");
+};
+
+class KbdRptParser : public KeyboardReportParser
+{
+    void PrintKey(uint8_t mod, uint8_t key);
+
+  protected:
+    void OnControlKeysChanged(uint8_t before, uint8_t after);
+    void OnKeyDown	(uint8_t mod, uint8_t key);
+    void OnKeyUp	(uint8_t mod, uint8_t key);
+    void OnKeyPressed(uint8_t key);
+};
+
+void KbdRptParser::PrintKey(uint8_t m, uint8_t key)
+{
+  MODIFIERKEYS mod;
+  *((uint8_t*)&mod) = m;
+  Serial.print((mod.bmLeftCtrl   == 1) ? "C" : " ");
+  Serial.print((mod.bmLeftShift  == 1) ? "S" : " ");
+  Serial.print((mod.bmLeftAlt    == 1) ? "A" : " ");
+  Serial.print((mod.bmLeftGUI    == 1) ? "G" : " ");
+
+  Serial.print(" >");
+  PrintHex<uint8_t>(key, 0x80);
+  Serial.print("< ");
+
+  Serial.print((mod.bmRightCtrl   == 1) ? "C" : " ");
+  Serial.print((mod.bmRightShift  == 1) ? "S" : " ");
+  Serial.print((mod.bmRightAlt    == 1) ? "A" : " ");
+  Serial.println((mod.bmRightGUI    == 1) ? "G" : " ");
+};
+
+void KbdRptParser::OnKeyDown(uint8_t mod, uint8_t key)
+{
+  Serial.print("DN ");
+  PrintKey(mod, key);
+  uint8_t c = OemToAscii(mod, key);
+
+  if (c)
+    OnKeyPressed(c);
+}
+
+void KbdRptParser::OnControlKeysChanged(uint8_t before, uint8_t after) {
+
+  MODIFIERKEYS beforeMod;
+  *((uint8_t*)&beforeMod) = before;
+
+  MODIFIERKEYS afterMod;
+  *((uint8_t*)&afterMod) = after;
+
+  if (beforeMod.bmLeftCtrl != afterMod.bmLeftCtrl) {
+    Serial.println("LeftCtrl changed");
+  }
+  if (beforeMod.bmLeftShift != afterMod.bmLeftShift) {
+    Serial.println("LeftShift changed");
+  }
+  if (beforeMod.bmLeftAlt != afterMod.bmLeftAlt) {
+    Serial.println("LeftAlt changed");
+  }
+  if (beforeMod.bmLeftGUI != afterMod.bmLeftGUI) {
+    Serial.println("LeftGUI changed");
+  }
+
+  if (beforeMod.bmRightCtrl != afterMod.bmRightCtrl) {
+    Serial.println("RightCtrl changed");
+  }
+  if (beforeMod.bmRightShift != afterMod.bmRightShift) {
+    Serial.println("RightShift changed");
+  }
+  if (beforeMod.bmRightAlt != afterMod.bmRightAlt) {
+    Serial.println("RightAlt changed");
+  }
+  if (beforeMod.bmRightGUI != afterMod.bmRightGUI) {
+    Serial.println("RightGUI changed");
+  }
+
+}
+
+void KbdRptParser::OnKeyUp(uint8_t mod, uint8_t key)
+{
+  Serial.print("UP ");
+  PrintKey(mod, key);
+}
+
+void KbdRptParser::OnKeyPressed(uint8_t key)
+{
+  Serial.print("ASCII: ");
+  Serial.println((char)key);
+};
+
+/*
+class HIDUniversal2 : public HIDUniversal
+{
+public:
+    HIDUniversal2(USB *usb) : HIDUniversal(usb) {};
+
+protected:
+    uint8_t OnInitSuccessful();
+};
+
+
+uint8_t HIDUniversal2::OnInitSuccessful()
+{
+    uint8_t    rcode;
+
+    HexDumper<USBReadParser, uint16_t, uint16_t>    Hex;
+    ReportDescParser                                Rpt;
+
+    if ((rcode = GetReportDescr(0, &Hex)))
+        goto FailGetReportDescr1;
+
+    if ((rcode = GetReportDescr(0, &Rpt)))
+	goto FailGetReportDescr2;
+
+    return 0;
+
+FailGetReportDescr1:
+    USBTRACE("GetReportDescr1:");
+    goto Fail;
+
+FailGetReportDescr2:
+    USBTRACE("GetReportDescr2:");
+    goto Fail;
+
+Fail:
+    Serial.println(rcode, HEX);
+    Release();
+    return rcode;
+}
+
+// Override HIDComposite to be able to select which interface we want to hook into
+class HIDSelector : public HIDComposite
+{
+public:
+    HIDSelector(USB *p) : HIDComposite(p) {};
+
+protected:
+    void ParseHIDData(USBHID *hid, uint8_t ep, bool is_rpt_id, uint8_t len, uint8_t *buf); // Called by the HIDComposite library
+    bool SelectInterface(uint8_t iface, uint8_t proto);
+};
+
+// Return true for the interface we want to hook into
+bool HIDSelector::SelectInterface(uint8_t iface, uint8_t proto)
+{
+  Serial.print("SelectInterface iface=");
+  Serial.print(iface);
+  Serial.print(", proto=");
+  Serial.println(proto);
+
+  if (proto != 0)
+    return true;
+
+  return false;
+}
+
+// Will be called for all HID data received from the USB interface
+void HIDSelector::ParseHIDData(USBHID *hid, uint8_t ep, bool is_rpt_id, uint8_t len, uint8_t *buf) {
+#if 1
+  if (len && buf)  {
+    Notify(PSTR("\r\n"), 0x80);
+    for (uint8_t i = 0; i < len; i++) {
+      D_PrintHex<uint8_t > (buf[i], 0x80);
+      Notify(PSTR(" "), 0x80);
+    }
+  }
+#endif
+}
+*/
+
+USB     Usb;
+USBHub     Hub(&Usb);
+USBHub     Hub2(&Usb);
+
+HIDBoot < USB_HID_PROTOCOL_KEYBOARD | USB_HID_PROTOCOL_MOUSE > HidComposite0(&Usb);
+HIDBoot < USB_HID_PROTOCOL_KEYBOARD | USB_HID_PROTOCOL_MOUSE > HidComposite1(&Usb);
+HIDBoot<USB_HID_PROTOCOL_KEYBOARD>    HidKeyboard(&Usb);
+HIDBoot<USB_HID_PROTOCOL_MOUSE>    HidMouse(&Usb);
+
+KbdRptParser KbdPrs;
+MouseRptParser MousePrs;
+
+// HIDSelector    hidSelector(&Usb);
+
+// HIDUniversal2 Hid0(&Usb);
+// HIDUniversal2 Hid1(&Usb);
+
+// UniversalReportParser Uni0;
+// UniversalReportParser Uni1;
+
+void setup()
+{
+    Serial.begin( 9600 );
+    Serial.println("Start1");
+
+    if (Usb.Init() == -1)
+    Serial.println("OSC did not start.");
+
+    UsbDEBUGlvl = 0xff;
+
+    delay( 200 );
+
+// if (!Hid0.SetReportParser(0, &Uni0))
+//       ErrorMessage<uint8_t>(PSTR("SetReportParser 0"), 1  );
+// if (!Hid1.SetReportParser(0, &Uni1))
+//       ErrorMessage<uint8_t>(PSTR("SetReportParser 1"), 1  );
+
+  HidComposite0.SetReportParser(1, &KbdPrs);
+  HidComposite0.SetReportParser(0, &MousePrs);
+  HidComposite1.SetReportParser(0, &KbdPrs);
+  HidComposite1.SetReportParser(1, &MousePrs);
+  HidKeyboard.SetReportParser(0, &KbdPrs);
+  HidMouse.SetReportParser(0, &MousePrs);
+
+  Serial.println("Start2");
+}
+
+void loop()
+{
+  Usb.Task();
+}
